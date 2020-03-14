ef9a8ce651d7fc1a759c3631e35ed79c2f3a2f54
juninholiveira <leandro-nim@hotmail.com>
2020-03-14 13:09:33 -0300

    EaseInOut added to BackToTop scroll

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,7 +3,7 @@ var button = document.getElementById("backToTop"),
     html = document.documentElement,
     offset = 100,
     scrollPos, 
-    docHeight,
+    docHeight;
 
 // Calculate the document Height and divide it by 4 to get the offset (uncessary, but cool)
 docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
@@ -14,19 +14,65 @@ if(docHeight != 'undefined')
 }
 
 //Add scroll event listener
-window.addEventListener("scroll", function(event)
-{
-    console.log("its working");
-    //.scrolTop gets the number of pixels already scrolled from the top of the page
-    scrollPos = body.scrollTop || html.scrollTop;
+window.addEventListener
+(
+    "scroll",
+    function(event)
+    {
+        console.log("its working");
+        //.scrolTop gets the number of pixels already scrolled from the top of the page
+        scrollPos = body.scrollTop || html.scrollTop;
 
-    //If the number of pixels already scrolled from the top of the page is greater than the offset, then adds the class 'visible' to the button, otherwise, removes it
-    button.className = (scrollPos > offset) ? "visible" : "";
-});
+        //If the number of pixels already scrolled from the top of the page is greater than the offset, then adds the class 'visible' to the button, otherwise, removes it
+        button.className = (scrollPos > offset) ? "visible" : "";
+    }
+);
 
 //Add click event listeners
-button.addEventListener("click", function(event)
+button.addEventListener
+(
+    "click",
+    function(event)
+    {
+        event.preventDefault();
+        scrollToTop(200,3);
+    }
+);
+
+function scrollToTop(totalTime, easingPower)
 {
-    event.preventDefault();
-    html.scrollTop = 0;
-});  
+    var timeInterval = 1; //in ms
+    var scrollTop = Math.round(body.scrollTop || html.scrollTop);
+    var timeLeft = totalTime;
+    var scrollByPixel = setInterval
+    (
+        function()
+        {
+            var percentSpent = (totalTime - timeLeft) / totalTime;
+            if(timeLeft >= 0)
+            {
+                var newScrollTop = scrollTop * (1 - easeInOut(percentSpent, easingPower));
+                body.scrollTop = newScrollTop;
+                html.scrollTop = newScrollTop;
+                timeLeft--;
+            }
+            else
+            {
+                clearInterval(scrollByPixel);
+            }
+        },
+        timeInterval
+    );
+}
+
+function easeInOut(t, power)
+{
+    if (t < 0.5)
+    {
+        return 0.5 * Math.pow(2* t, power);
+    }
+    else
+    {
+        return 0.5 * (2 - Math.pow(2 * (1 - t), power));
+    }
+}

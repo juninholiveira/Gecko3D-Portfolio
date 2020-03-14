86d8a0a3cd076b9d5c3c1edbe8f40710e6de22ec
juninholiveira <leandro-nim@hotmail.com>
2020-03-14 12:52:13 -0300

    JQuery removed from BackToTop

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,32 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 40){
-            $('#backToTop').fadeIn();
-        }
-        else{
-            $('#backToTop').fadeOut();
-        }
-    });       
+var button = document.getElementById("backToTop"),
+    body = document.body,
+    html = document.documentElement,
+    offset = 100,
+    scrollPos, 
+    docHeight,
 
-    $("#backToTop").click(function(){
-        $('html ,body').animate({scrollTop : 0},800);
-    });
+// Calculate the document Height and divide it by 4 to get the offset (uncessary, but cool)
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+if(docHeight != 'undefined')
+{
+    //The offset needed to the button show up will be the height of the page divided by 4
+    offset = docHeight / 4;
+}
+
+//Add scroll event listener
+window.addEventListener("scroll", function(event)
+{
+    console.log("its working");
+    //.scrolTop gets the number of pixels already scrolled from the top of the page
+    scrollPos = body.scrollTop || html.scrollTop;
+
+    //If the number of pixels already scrolled from the top of the page is greater than the offset, then adds the class 'visible' to the button, otherwise, removes it
+    button.className = (scrollPos > offset) ? "visible" : "";
 });
+
+//Add click event listeners
+button.addEventListener("click", function(event)
+{
+    event.preventDefault();
+    html.scrollTop = 0;
+});  
